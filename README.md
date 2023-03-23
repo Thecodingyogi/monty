c4bc511659086734dd0885a28d58e88cad74f50d
Thecodingyogi <cynthiatonui@gmail.com>
2023-03-23 13:44:20 -0700

    Monty tasks

---
+++ README.md
@@ -0,0 +1,1 @@
+All tasks of 0x19. C - Stacks, Queues - LIFO, FIFO
---
+++ monty.h
@@ -0,0 +1,67 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct bus_s - opcoode and its function
+ * @file: the opcode
+ * @arg: value
+ * @content: line content
+ * @lifi: flag change stack - queue
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct bus_s
+{
+	FILE *file;
+	char *arg;
+	char *content;
+	int lifi;
+} bus_t;
+
+extern bus_t bus;
+
+void f_pint(stack_t **head, unsigned int counter);
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+
+#endif /* MONTY_H */
